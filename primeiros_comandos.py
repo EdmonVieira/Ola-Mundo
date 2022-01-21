60a645a44b33f16c634b0ced2d8bebb2345d69ab
Edmon Vieira <edmonnv90@gmail.com>
2022-01-20 22:16:45 -0300

    Class Updates

---
+++ primeiros_comandos.py
@@ -0,0 +1,25 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+print('Olá',5)
+
+nome = input('Qual é seu nome?')
+idade = input('Qual é sua idade?')
+peso = input('Qual é seu peso?')
+print(nome,idade,peso)
+
+#Desafio chefão - 1
+nome = input('Qual é seu nome?')
+print('Olá ' + nome + ' ! Prazer em te conhecer!')
+
+#Desafio chefão - 2
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+print('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'. Correto?')
+
+#Desafio chefão - 3
+primeiro = int(input('Primeiro número: '))
+segundo = int(input('Segundo número: '))
+soma = primeiro+segundo
+print('A soma é',soma)
